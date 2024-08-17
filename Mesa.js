663a06a0259aadd03d4328dc9a91b6642270515c
Gabriel Soares <64909008+gabrielsoaressn@users.noreply.github.com>
2024-08-17 18:52:02 -0300

    Update na classe Mesa
    Coloquei um array de clientes em mesa

--- Mesa.js
+++ Mesa.js
@@ -1,6 +1,8 @@
 class Mesa {
-    constructor(id, numero, status) {
+    constructor(id, numero, status, clientes = []) {
         this.id = id;
         this.numero = numero;
+        this.status = status; // Ex: 'Livre', 'Ocupada', 'Reservada' o que achas?
+        this.clientes = clientes; // Array de objetos do tipo Cliente
     }
 }
