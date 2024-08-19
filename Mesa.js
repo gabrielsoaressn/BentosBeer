29275c874c78788099a91fd5967f30b17b0e73d8
Gabriel Soares <64909008+gabrielsoaressn@users.noreply.github.com>
2024-08-19 15:46:56 -0300

    Delete Mesa.js

--- Mesa.js
+++
@@ -1,8 +0,0 @@
-class Mesa {
-    constructor(id, numero, status, clientes = []) {
-        this.id = id;
-        this.numero = numero;
-        this.status = status; // Ex: 'Livre', 'Ocupada', 'Reservada' o que achas?
-        this.clientes = clientes; // Array de objetos do tipo Cliente
-    }
-}
