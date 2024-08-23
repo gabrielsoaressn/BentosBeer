339b29282b1e8ec763d8ce57202a5e3f1d2ac7f5
Rafael Batista <97126688+RafaBatista1@users.noreply.github.com>
2024-08-23 18:45:59 -0300

    Produto.js

---
+++ Teste1/Produto.js
@@ -0,0 +1,9 @@
+class Produto {
+    constructor(id, nome, preco) {
+        this.id = id; // ID único do produto
+        this.nome = nome; // Nome do produto
+        this.preco = preco; // Preço do produto
+    }
+}
+
+export default Produto;
